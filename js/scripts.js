3a9216f4699849ae09f9fbfca23765ff540694a0
Renee Mei and Shruti Priya <shruti@>
2017-01-17 10:45:57 -0800

    add second checkbox form

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,11 @@ $("form#transportation_survey").submit(function(event){
     var workTransportMode = $(this).val();
     $("#work-responses").append(workTransportMode + "<br>");
    });
+  $("#fun-responses").show();
+  $("input:checkbox[name=fun-transportation]:checked").each(function(){
+    var funTransportationMode = $(this).val();
+    $("#fun-responses").append(funTransportationMode + "<br>");
+  });
   $("#transportation_survey").hide();
 });
 
